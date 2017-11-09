5a7854bc9a1ab4e6aa9332c1ec4a6110b4e721c4
Antonio Medrano <tonedabass@gmail.com>
2017-11-08 19:59:54 -0800

    now reads TSPLIB files as well

--- plot.py
+++ plot.py
@@ -19,19 +19,23 @@ import numpy as np
 
 def plotData(sites):
     
+    mSize = 4
+    
     plt.figure(figsize=(6,6))
-    plt.plot(sites[:,1], sites[:,2], 'ko')
+    plt.plot(sites[:,1], sites[:,2], 'ko', markersize = mSize)
     plt.axis('equal')
     plt.show()
 
 def plotSolution(sites, X, cols, SD):
     
+    mSize = 4
+    
     plt.figure(figsize=(6,6))
-    plt.plot(sites[:,1], sites[:,2], 'ko')
+    plt.plot(sites[:,1], sites[:,2], 'ko', markersize = mSize)
     numSites = len(X)
     for j in range(numSites):
         if (X[j].SolutionValue() == 1.0):
-            plt.plot(sites[cols[j],1], sites[cols[j],2], 'bo')
+            plt.plot(sites[cols[j],1], sites[cols[j],2], 'bo', markersize = mSize)
             circle = plt.Circle((sites[cols[j],1], sites[cols[j],2]), SD, color='b', fill=False)
             plt.gcf().gca().add_artist(circle)
     
@@ -40,16 +44,18 @@ def plotSolution(sites, X, cols, SD):
     
 def plotSolutionE(sites, essential, X, cols, SD):
     
+    mSize = 4
+    
     plt.figure(figsize=(6,6))
-    plt.plot(sites[:,1], sites[:,2], 'ko')
+    plt.plot(sites[:,1], sites[:,2], 'ko', markersize = mSize)
     numSites = len(X)
     for j in range(numSites):
         if (X[j].SolutionValue() == 1.0):
-            plt.plot(sites[cols[j],1], sites[cols[j],2], 'bo')
+            plt.plot(sites[cols[j],1], sites[cols[j],2], 'bo', markersize = mSize)
             circle = plt.Circle((sites[cols[j],1], sites[cols[j],2]), SD, color='b', fill=False)
             plt.gcf().gca().add_artist(circle)
     for j in essential:
-        plt.plot(sites[j,1], sites[j,2], 'ro')
+        plt.plot(sites[j,1], sites[j,2], 'ro', markersize = mSize)
         circle = plt.Circle((sites[j,1], sites[j,2]), SD, color='b', fill=False)
         plt.gcf().gca().add_artist(circle)
     
--- readDataFiles.py
+++ readDataFiles.py
@@ -14,6 +14,7 @@
 # Author: Antonio Medrano
 
 import sys
+import re
 import numpy as np
 
 def readDat(file):
@@ -59,6 +60,55 @@ def readTSP(file):
     """
     Read a file from the TSP library .tsp file format
     """
-    sites = 0
     
+    data = read_tsp_data(file)
+    numSites = int(detect_dimension(data))
+    sites = np.ones([numSites,4])        
+    i = 0
+    dataPortion = False
+    for line in data:
+        if dataPortion == True:
+            row = line.split()
+            # Set constraint coefficients
+            #print row
+            sites[i,0:3] = row[0:3]
+            i += 1
+            if i == numSites:
+                break
+        else:
+            if line.startswith('NODE_COORD_SECTION'):
+                dataPortion = True
+                continue
     return sites
+    
+    
+def read_tsp_data(tsp_name):
+	tsp_name = tsp_name
+	with open(tsp_name) as f:
+		content = f.read().splitlines()
+		cleaned = [x.lstrip() for x in content if x != ""]
+		return cleaned
+"""
+We return a list like 
+['NAME: ulysses16.tsp',
+'TYPE: TSP',
+'COMMENT: Odyssey of Ulysses (Groetschel/Padberg)',
+'DIMENSION: 16',
+'EDGE_WEIGHT_TYPE: GEO',
+'DISPLAY_DATA_TYPE: COORD_DISPLAY',
+'NODE_COORD_SECTION',
+'1 38.24 20.42',
+'2 39.57 26.15',
+'3 40.56 25.32',
+................
+'EOF']
+"""
+
+"""
+Check for the line DIMENSION in the file and keeps the numeric value
+"""
+def detect_dimension(in_list):
+	non_numeric = re.compile(r'[^\d]+')
+	for element in in_list:
+		if element.startswith("DIMENSION"):
+			return non_numeric.sub("",element)
