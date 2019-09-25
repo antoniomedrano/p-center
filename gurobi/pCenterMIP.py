a53a87a58972326296f371507f59c85e944ed828
Antonio Medrano <tonedabass@gmail.com>
2019-09-25 17:32:15 -0500

    update copyrights

--- gurobi/pCenterMIP.py
+++ gurobi/pCenterMIP.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Antonio Medrano
+# Copyright 2019 Antonio Medrano
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
